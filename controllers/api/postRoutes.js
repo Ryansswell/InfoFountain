835a550c1c4cd7867c11a01a511595e5256e636f
Ryan Anderson <ryan0425@msn.com>
2021-04-30 12:43:33 -0700

    the updated postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -17,17 +17,6 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newPost = await Post.edit({
-
-    });
-
-    res.status(200).json(newPost);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
@@ -44,6 +33,42 @@ router.delete('/:id', withAuth, async (req, res) => {
     }
 
     res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/commentPost', withAuth, async (req, res) => {
+  try {
+    const commentPost = await Comment.create(req.body);
+
+    req.session.save(() => {
+      req.session.commentPost_id = commentPost.id;
+      req.session.logged_in = true;
+    });
+
+    res.status(200).json(commentPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.delete('/comment/:id', withAuth, async (req, res) => {
+  try {
+    const postDelete = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        post_id: req.session.postdelete_id,
+      },
+    });
+
+    if (!postDelete) {
+      res.status(404).json({ message: 'This post is deleted!' });
+      return;
+    }
+
+    res.status(200).json(postDelete);
   } catch (err) {
     res.status(500).json(err);
   }
